75191a006ef8743d56144cd2e6fb0977d20fdbbf
55rah <rahulshetty2k6@gmail.com>
2023-11-14 12:58:51 +1300

    Added Payroll validation error message

--- sys/Models/Payroll.cs
+++ sys/Models/Payroll.cs
@@ -10,19 +10,19 @@ namespace PMS10.Models
 
         //Foreign key for Employee
         [ForeignKey("Employee")]
-        [RegularExpression("^[1-9]+[0-9]*$")]
+        [RegularExpression("^[1-9]+[0-9]*$", ErrorMessage = "Please enter a valid ID for an employee record")]
         public int Employee_ID { get; set; }
         public Employee? Employee { get; set; }
 
         //Foreign key for Salary
         [ForeignKey("Salaries")]
-        [RegularExpression("^[1-9]+[0-9]*$")]
+        [RegularExpression("^[1-9]+[0-9]*$", ErrorMessage = "Please enter a valid ID for an salary record")]
         public int Salary_ID { get; set; }
         public Salaries? Salaries { get; set; }
 
         //Foreign key for Shift
         [ForeignKey("Shift")]
-        [RegularExpression("^[1-9]+[0-9]*$")]
+        [RegularExpression("^[1-9]+[0-9]*$", ErrorMessage = "Please enter a valid ID for an shift record")]
         public int Shift_ID { get; set; }
         public Shift? Shift { get; set; }
 
@@ -34,7 +34,7 @@ namespace PMS10.Models
         // This [Column(TypeName = "decimal(7,2)")] defines the decimal field for TotalAmount where 7 is the allowed amount of total digits in a number and 2 is the amont after a decimal
         [Column(TypeName = "decimal(19,2)")]
         [DisplayName("Total Amount($)")]
-        [RegularExpression("^\\$?([1-9]{1}[0-9]{0,2}(\\,[0-9]{3})*(\\.[0-9]{0,2})?|[1-9]{1}[0-9]{0,}(\\.[0-9]{0,2})?|0(\\.[0-9]{0,2})?|(\\.[0-9]{1,2})?)$")]
+        [RegularExpression("^\\$?([1-9]{1}[0-9]{0,2}(\\,[0-9]{3})*(\\.[0-9]{0,2})?|[1-9]{1}[0-9]{0,}(\\.[0-9]{0,2})?|0(\\.[0-9]{0,2})?|(\\.[0-9]{1,2})?)$", ErrorMessage = "Please enter a valid amount of money")]
         public decimal TotalAmount { get; set; }
     }
 }
