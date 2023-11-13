96c15f6f0f7147fc03344208f6914656014fa759
55rah <rahulshetty2k6@gmail.com>
2023-11-14 12:49:30 +1300

    Fixed Payroll validation

--- sys/Models/Payroll.cs
+++ sys/Models/Payroll.cs
@@ -10,16 +10,19 @@ namespace PMS10.Models
 
         //Foreign key for Employee
         [ForeignKey("Employee")]
+        [RegularExpression("^[1-9]+[0-9]*$")]
         public int Employee_ID { get; set; }
         public Employee? Employee { get; set; }
 
         //Foreign key for Salary
         [ForeignKey("Salaries")]
+        [RegularExpression("^[1-9]+[0-9]*$")]
         public int Salary_ID { get; set; }
         public Salaries? Salaries { get; set; }
 
         //Foreign key for Shift
         [ForeignKey("Shift")]
+        [RegularExpression("^[1-9]+[0-9]*$")]
         public int Shift_ID { get; set; }
         public Shift? Shift { get; set; }
 
@@ -31,6 +34,7 @@ namespace PMS10.Models
         // This [Column(TypeName = "decimal(7,2)")] defines the decimal field for TotalAmount where 7 is the allowed amount of total digits in a number and 2 is the amont after a decimal
         [Column(TypeName = "decimal(19,2)")]
         [DisplayName("Total Amount($)")]
+        [RegularExpression("^\\$?([1-9]{1}[0-9]{0,2}(\\,[0-9]{3})*(\\.[0-9]{0,2})?|[1-9]{1}[0-9]{0,}(\\.[0-9]{0,2})?|0(\\.[0-9]{0,2})?|(\\.[0-9]{1,2})?)$")]
         public decimal TotalAmount { get; set; }
     }
 }
