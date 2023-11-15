a9d09f7c03d1e12c384412b9a9d99f2a82c20533
55rah <rahulshetty2k6@gmail.com>
2023-11-15 20:58:47 +1300

    Added validation and comments for Salaries and Shift

--- sys/Models/Salaries.cs
+++ sys/Models/Salaries.cs
@@ -19,12 +19,16 @@ namespace PMS10.Models
     {
         [Key] public int Salary_ID { get; set; }
 
+        // This regular expression only allows values of 1.00-99999999999999999.99 while allowing the optional input of 0,1 or 2 decimal points
         [Column(TypeName = "decimal(19,2)")]
         [DisplayName("Salary($)")]
+        [RegularExpression("^(?!0\\.00$)([1-9]\\d{0,16}(\\.\\d{1,2})?)$", ErrorMessage = "Please input a valid amount of money")]
         public decimal Salary { get; set; }
 
+        // This regular expression only allows values of 1.00-999.99 while allowing the optional input of 0,1 or 2 decimal points
         [Column(TypeName = "decimal(5,2)")]
         [DisplayName("Wage($)")]
+        [RegularExpression("^(?!0\\.00$)([1-9]\\d{0,2}(\\.\\d{1,2})?)$", ErrorMessage = "Please input a valid amount of money")]
         public decimal Wage { get; set; }
         public Bonus Bonus { get; set; }
         public ICollection<Payroll> Payrolls { get; } = new List<Payroll>();
--- sys/Models/Shift.cs
+++ sys/Models/Shift.cs
@@ -7,7 +7,9 @@ namespace PMS10.Models
     {
         [Key] public int Shift_ID { get; set; }
 
+        // This regular expression only allows values of 1.00-99.99 while allowing the optional input of 0,1 or 2 decimal points
         [Column(TypeName = "decimal(4,2)")]
+        [RegularExpression("^(?!0\\\\.00)([1-9]\\d?(\\.\\d{1,2})?)$", ErrorMessage = "Please enter a valid number")]
         public decimal Hours { get; set; }
         public ICollection<Payroll> Payrolls { get; } = new List<Payroll>();
     }
