28baaf9e509bc155aa740df490c2ddb997729a91
55rah <rahulshetty2k6@gmail.com>
2023-11-16 17:24:09 +1300

    Shift range validation

--- sys/Models/Shift.cs
+++ sys/Models/Shift.cs
@@ -9,7 +9,7 @@ namespace PMS10.Models
 
         // This regular expression only allows values of 1.00-99.99 while allowing the optional input of 0,1 or 2 decimal points
         [Column(TypeName = "decimal(4,2)")]
-        [RegularExpression("^(?!0\\\\.00)([1-9]\\d?(\\.\\d{1,2})?)$", ErrorMessage = "Please enter a valid number")]
+        [Range(1,24)]
         public decimal Hours { get; set; }
         public ICollection<Payroll> Payrolls { get; } = new List<Payroll>();
     }
